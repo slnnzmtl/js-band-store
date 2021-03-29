b3e5d8404032e507a6bd807bd077878ab82db28c
unknown <47865690+slnnzmtl@users.noreply.github.com>
2021-03-29 15:00:23 +0300

    update Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
-import CartButton from "../CartButton/CartButton";
-import "./Header.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CartButton from '../CartButton/CartButton';
+import './Header.scss';
 
-const Header = () => {
-
-  return (
-    <div className="header">
-      <div className="header__container">
+const Header = () => (
+  <div className="header">
+    <div className="header__container">
+      <Link to="/">
         <span className="header__title">JS Band Store</span>
-        <CartButton />
-      </div>
+      </Link>
+      <CartButton />
     </div>
-  )
-};
-
+  </div>
+);
 
 export default Header;
