decb85c637dcfcbf4c366ed015e4cabb30e31a3f
unknown <47865690+slnnzmtl@users.noreply.github.com>
2021-03-29 14:59:52 +0300

    create Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => (
+  <div className="loader-wrapper">
+    <div className="loader">Loading...</div>
+  </div>
+);
+
+export default Loader;
