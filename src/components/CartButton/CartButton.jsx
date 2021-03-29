9045bc8ad19bd0276044cac27eb4c1df187eee57
unknown <47865690+slnnzmtl@users.noreply.github.com>
2021-03-29 15:01:57 +0300

    Update CartButton.jsx

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,12 +1,25 @@
-import icon from "../../assets/icons/cart.png";
-import "./CartButton.scss";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import icon from '../../assets/icons/cart.png';
+import './CartButton.scss';
 
-const CartButton = () => (
-  <div className="cart-button">
-    <img src={icon} alt="Cart icon" className="cart-button__img" />
-    <span className="cart-button__span">Cart</span>
-    <span className="cart-counter cart-button__span">(3)</span>
-  </div>
-);
+const CartButton = () => {
+  const { list } = useSelector((state) => state.cart);
+
+  return (
+    <Link to="/cart">
+      <div className="cart-button">
+        <img src={icon} alt="Cart icon" className="cart-button__img" />
+        <span className="cart-button__span">Cart</span>
+        <span className="cart-counter cart-button__span">
+          (
+          {list.length}
+          )
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 export default CartButton;
